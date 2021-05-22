95c10b70d94f314b1e5795a12830d14feda44285
HenryGillard <>
2021-05-22 23:13:31 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+
+//Add 2 numbers//
+function add(sum1,sum2) {
+  return sum1 + sum2;
+
+}
+//multiply 2 numbers//
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+//Divide 2 numbers//
+function divide(num1, num2) {
+  return num1 / num2;
+}
+//Convert Fahrenheit to Celsius//
+function convertFtoC(fahrenheit) {
+  return (fahrenheit - 32) * 5/9
+}
+//Convert Celsius to Fahrenheit//
+function convertCtoF(celsius) {
+ return (celsius * 9/5) + 32;
+}
+
+
